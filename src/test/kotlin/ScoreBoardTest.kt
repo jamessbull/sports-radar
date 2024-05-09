0f77605252548b42e176acd67c01f2368c36e61e
Jim <james.s.bull@gmail.com>
2024-05-09 17:06:54 +0100

    Check in pom with dependencies for junit 5 and hamkrest for matching plus simple test to prove I have what I need to begin.

---
+++ src/test/kotlin/ScoreBoardTest.kt
@@ -0,0 +1,10 @@
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Test
+
+class ScoreBoardTest {
+    @Test
+    fun `check I can write tests`() {
+       assertThat(1+1, equalTo(2))
+    }
+}
